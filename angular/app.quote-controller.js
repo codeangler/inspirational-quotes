034548cac6407875ea5ba9261b8e81ffb4d3a40e
codeangler <burnett.casey@gmail.com>
2016-06-10 16:04:09 -0600

    submitQuote() passing object to array w/n factory || add debugging button to return current factory array

--- angular/app.quote-controller.js
+++ angular/app.quote-controller.js
@@ -6,17 +6,20 @@ angular.module('quoteApp')
   function qCtrl(QuoteFactory) {
     var qCtrl = this;
     qCtrl.author = "";
-    qCtrl.aquote = "";
+    qCtrl.quote = "";
     qCtrl.rating = "";
     qCtrl.quoteBlock = QuoteFactory;
     console.log(qCtrl.quoteBlock);
 
     qCtrl.submitQuote = function(){
-      // qCtrl.quoteBlock.push({qCtrl.quote, qCtrl.author, qCtrl.rating})
+      qCtrl.quoteBlock.push({quote:qCtrl.quote, author:qCtrl.author, rating:qCtrl.rating})
       qCtrl.author = "";
       qCtrl.quote = "";
       qCtrl.rating = "";
       console.log(qCtrl.quoteBlock)
+    }
 
+    qCtrl.quoteArr = function(){
+      console.log(qCtrl.quoteBlock);
     }
   }
